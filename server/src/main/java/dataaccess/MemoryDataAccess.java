8d602844ca4f0b7dfd92fab3f6183c9f76d0a684
Jinho Lee <jinholee@byu.edu>
2026-07-14 23:36:46 -0600

    added DataAccess interface and memory implementation

--- server/src/main/java/dataaccess/MemoryDataAccess.java
+++ server/src/main/java/dataaccess/MemoryDataAccess.java
@@ -1,4 +1,67 @@
 package dataaccess;
 
-public class MemoryDataAccess {
+import model.AuthData;
+import model.GameData;
+import model.UserData;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MemoryDataAccess implements DataAccess {
+    private final Map<String, UserData> users = new HashMap<>();
+    private final Map<String, AuthData> auths = new HashMap<>();
+    private final Map<Integer, GameData> games = new HashMap<>();
+
+    @Override
+    public void clear() throws DataAccessException {
+        users.clear();
+        auths.clear();
+        games.clear();
+    }
+
+    @Override
+    public void createUser(UserData user) throws DataAccessException {
+        users.put(user.username(), user);
+    }
+
+    @Override
+    public UserData getUser(String username) throws DataAccessException {
+        return users.get(username);
+    }
+
+    @Override
+    public void createGame(GameData game) throws DataAccessException {
+        games.put(game.gameID(), game);
+    }
+
+    @Override
+    public GameData getGame(int gameID) throws DataAccessException {
+        return games.get(gameID);
+    }
+
+    @Override
+    public Collection<GameData> listGames() throws DataAccessException {
+        return games.values();
+    }
+
+    @Override
+    public void updateGame(GameData game) throws DataAccessException {
+        games.put(game.gameID(), game);
+    }
+
+    @Override
+    public void createAuth(AuthData auth) throws DataAccessException {
+        auths.put(auth.authToken(), auth);
+    }
+
+    @Override
+    public AuthData getAuth(String authToken) throws DataAccessException {
+        return auths.get(authToken);
+    }
+
+    @Override
+    public void deleteAuth(String authToken) throws DataAccessException {
+        auths.remove(authToken);
+    }
 }
