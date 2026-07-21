e6a4e3fad2e45c0c09602c9dd2a4b74ac5ab0e86
Jinho Lee <jinholee@byu.edu>
2026-07-20 22:29:18 -0600

    added createUser

--- server/src/main/java/dataaccess/MySqlDataAccess.java
+++ server/src/main/java/dataaccess/MySqlDataAccess.java
@@ -67,8 +67,17 @@ public class MySqlDataAccess implements DataAccess{
 
     @Override
     public void createUser(UserData user) throws DataAccessException {
-        
-
+        String hashed = org.mindrot.jbcrypt.BCrypt.hashpw(user.password(), org.mindrot.jbcrypt.BCrypt.gensalt());
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var ps = conn.prepareStatement("INSERT INTO user (username, password, email) VALUES (?, ?, ?)")) {
+                ps.setString(1, user.username());
+                ps.setString(2, hashed);
+                ps.setString(3, user.email());
+                ps.executeUpdate();
+            }
+        } catch (java.sql.SQLException ex) {
+            throw new DataAccessException("Unable to create user", ex);
+        }
     }
 
     @Override
