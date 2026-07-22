589b690050eff8fb312541ecc079c761b281b5d4
Jinho Lee <jinholee@byu.edu>
2026-07-21 23:09:05 -0600

    added game DAO unit tests

--- server/src/test/java/dataaccess/MySqlDataAccessTests.java
+++ server/src/test/java/dataaccess/MySqlDataAccessTests.java
@@ -1,5 +1,6 @@
 package dataaccess;
 
+import chess.ChessGame;
 import model.*;
 import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
@@ -71,4 +72,61 @@ public class MySqlDataAccessTests {
     public void deleteAuthNotFound() {
         assertDoesNotThrow(() -> dataAccess.deleteAuth("nothing"));
     }
+
+    @Test
+    public void createGameSuccess() throws DataAccessException {
+        dataAccess.createGame(new GameData(1234, null, null, "test", new chess.ChessGame()));
+        assertNotNull(dataAccess.getGame(1234));
+    }
+
+    @Test
+    public void createGameDuplicate() throws DataAccessException {
+        dataAccess.createGame(new GameData(1234, null, null, "test", new chess.ChessGame()));
+        assertThrows(DataAccessException.class,
+                () -> dataAccess.createGame(new GameData(1234, null, null,  "test", new chess.ChessGame())));
+    }
+
+    @Test
+    public void getGameSuccess() throws DataAccessException {
+        dataAccess.createGame(new GameData(1234, null, null, "test", new chess.ChessGame()));
+        assertEquals("test", dataAccess.getGame(1234).gameName());
+    }
+
+    @Test
+    public void getGameNotFound() throws DataAccessException {
+        assertNull(dataAccess.getGame(9999));
+    }
+
+    @Test
+    public void listGameSuccess() throws DataAccessException {
+        dataAccess.createGame(new GameData(1234, null, null, "test1", new chess.ChessGame()));
+        dataAccess.createGame(new GameData(5678, null, null, "test2", new chess.ChessGame()));
+        assertEquals(2, dataAccess.listGames().size());
+    }
+
+    @Test
+    public void listGamesEmpty() throws DataAccessException {
+        assertEquals(0, dataAccess.listGames().size());
+    }
+
+    @Test
+    public void updateGameSuccess() throws DataAccessException {
+        dataAccess.createGame(new GameData(1234, null, null, "test", new chess.ChessGame()));
+        dataAccess.updateGame(new GameData(1234, "alice", null, "test", new chess.ChessGame()));
+        assertEquals("alice", dataAccess.getGame(1234).whiteUsername());
+    }
+
+    @Test
+    public void updateGameNotFound() {
+        assertDoesNotThrow(()->dataAccess.updateGame(
+                new GameData(9999, null, null, "none", new chess.ChessGame())));
+    }
+
+    @Test
+    public void clearSuccess() throws DataAccessException {
+        dataAccess.createUser(new UserData("alice", "hashed", "a@b.com"));
+        dataAccess.clear();
+        assertNull(dataAccess.getUser("alice"));
+    }
+
 }
