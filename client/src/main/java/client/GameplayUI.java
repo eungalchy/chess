7d2accf009a52236915bc5830d432665c2e2fd49
Jinho Lee <jinholee@byu.edu>
2026-08-06 07:29:27 -0600

    phase 6 complete: full websocket integration working

--- client/src/main/java/client/GameplayUI.java
+++ client/src/main/java/client/GameplayUI.java
@@ -1,14 +1,38 @@
 package client;
 
+import com.google.gson.Gson;
+import websocket.commands.ConnectCommand;
+import websocket.messages.LoadGameMessage;
+import websocket.messages.NotificationMessage;
+import websocket.messages.ErrorMessage;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.WebSocket;
 import java.util.Scanner;
+import java.util.concurrent.CompletionStage;
 
 public class GameplayUI {
+    private final WebSocket webSocket;
     private final String authToken;
     private final int gameID;
+    private final Gson gson = new Gson();
 
     public GameplayUI(String serverUrl, String authToken, int gameID) throws Exception {
         this.authToken = authToken;
         this.gameID = gameID;
+
+        String wsUrl = serverUrl.replace("http", "ws") + "/ws";
+        HttpClient client = HttpClient.newHttpClient();
+        this.webSocket = client.newWebSocketBuilder()
+                .buildAsync(URI.create(wsUrl), new WebSocketListener())
+                .join();
+
+        Thread.sleep(500);
+
+        ConnectCommand connect = new ConnectCommand(authToken, gameID);
+        webSocket.sendText(gson.toJson(connect), true);
+
+        Thread.sleep(500);
     }
 
     public void run() {
@@ -39,11 +63,14 @@ public class GameplayUI {
                     break;
                 case "resign":
                     System.out.println("You resigned from the game.");
+                    webSocket.sendClose(WebSocket.NORMAL_CLOSURE, "resign");
                     return;
                 case "leave":
                     System.out.println("You left the game.");
+                    webSocket.sendClose(WebSocket.NORMAL_CLOSURE, "leave");
                     return;
                 case "quit":
+                    webSocket.sendClose(WebSocket.NORMAL_CLOSURE, "quit");
                     return;
                 default:
                     System.out.println("Unknown command. Type help for options.");
@@ -58,5 +85,23 @@ public class GameplayUI {
         System.out.println("  leave - leave game");
         System.out.println("  quit - exit");
         System.out.println("  help - show this help");
+    }
+
+    private class WebSocketListener implements WebSocket.Listener {
+        @Override
+        public void onOpen(WebSocket webSocket) {
+            System.out.println("Connected to game server");
+        }
+
+        @Override
+        public CompletionStage<?> onText(WebSocket webSocket, CharSequence data, boolean last) {
+            System.out.println("Server: " + data);
+            return null;
+        }
+
+        @Override
+        public void onError(WebSocket webSocket, Throwable error) {
+            System.out.println("Error: " + error.getMessage());
+        }
     }
 }
